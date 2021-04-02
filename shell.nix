e661c5471cfc7a69acd34f4496c605ba21aa77bc
l-d-s <l@sdf.org>
2021-04-01 22:49:21 -0400

    Quick attempt at Q1-3

---
+++ shell.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
+    let
+    
+    includedPackages = with pkgs.rPackages; [
+                tidyverse
+                broom
+
+                cowplot
+                lemon
+
+                devtools
+                knitr
+
+                ];
+    
+    RstudioWithPackages =
+        pkgs.rstudioWrapper.override{
+            packages = includedPackages;
+        };
+        
+    RWithPackages =
+        pkgs.rWrapper.override{
+            packages = includedPackages;
+        };
+    in
+
+    pkgs.mkShell {
+        buildInputs = with pkgs; [
+            RstudioWithPackages 
+            RWithPackages
+            # libintl 
+            # libiconv
+            pandoc
+            ];
+        shellHook = ''
+            mkdir -p "$(pwd)/_libs"
+            export R_LIBS_USER="$(pwd)/_libs"
+        '';
+        }
